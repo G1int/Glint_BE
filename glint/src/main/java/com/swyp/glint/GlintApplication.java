7a4e4f7af38bd2c4fc1349a24097ab79e3b3e52c
yssy3135 <ys3135@gmail.com>
2024-06-22 17:02:12 +0900

    feat: #1 : 프로젝트 init

---
+++ glint/src/main/java/com/swyp/glint/GlintApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.glint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlintApplication.class, args);
+    }
+
+}
---
+++ glint/src/test/java/com/swyp/glint/GlintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp.glint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
