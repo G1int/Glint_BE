bdd280af2271140befb877717769d740859da20b
yssy3135 <ys3135@gmail.com>
2024-07-01 21:43:50 +0900

    fix: repository 어노테이션 추가

--- glint/src/main/java/com/swyp/glint/user/repository/UserRepository.java
+++ glint/src/main/java/com/swyp/glint/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.swyp.glint.user.repository;
 import com.swyp.glint.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(
