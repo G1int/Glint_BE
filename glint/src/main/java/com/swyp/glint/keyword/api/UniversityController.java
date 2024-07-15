bf42cc5e4b7c4ecd642d1e34a68f91056a3e8fe3
sunny.park <seolseee@gmail.com>
2024-07-15 22:51:06 +0900

    fix : 회원 프로필 - 대학(University) create 함수 수정 및 속성(학과) 추가 및 기타 수정

--- glint/src/main/java/com/swyp/glint/keyword/api/UniversityController.java
+++ glint/src/main/java/com/swyp/glint/keyword/api/UniversityController.java
@@ -4,8 +4,11 @@ import com.swyp.glint.keyword.application.UniversityService;
 import com.swyp.glint.keyword.domain.University;
 import com.swyp.glint.keyword.domain.UniversityCategory;
 import com.swyp.glint.common.exception.NotFoundEntityException;
+import com.swyp.glint.keyword.domain.Work;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,18 +33,36 @@ public class UniversityController {
         return ResponseEntity.ok(universityService.getAllUniversity());
     }
 
-    @GetMapping("/{universityId}")
-    @Operation(summary = "Get a university by its ID", description = "University Id를 통학 대학 조회")
+    @GetMapping("/university")
+    @Operation(summary = "Get a university by its Name", description = "대학명을 통한 대학 조회")
+    public ResponseEntity<List<University>> getUniversityByName(@RequestParam String universityName) {
+        List<University> universities = universityService.getUniversitiesByName(universityName);
+        return ResponseEntity.ok(universities);
+    }
+
+    @GetMapping("/{universityId}/university")
+    @Operation(summary = "Get a university by its ID", description = "University Id를 통한 대학 조회")
     public ResponseEntity<University> getUniversityById(@PathVariable Long universityId) {
         University university = universityService.findById(universityId);
         return ResponseEntity.ok(university);
     }
 
-    @GetMapping("/by-name")
-    @Operation(summary = "Get a university by its Name", description = "대학명을 통학 대학 조회")
-    public ResponseEntity<University> getUniversityByName(@RequestParam String universityName) {
-        University university = universityService.findByName(universityName);
-        return ResponseEntity.ok(university);
+    @GetMapping("/categories")
+    @Operation(summary = "List all university categories", description = "모든 대학 카테고리 조회")
+    public ResponseEntity<List<UniversityCategory>> getAllUniversityCategories() {
+        return ResponseEntity.ok(universityService.getAllUniversityCategory());
+    }
+
+    @GetMapping("/category")
+    @Operation(summary = "Get a university category by its university name", description = "대학명을 통한 카테고리 조회")
+    public ResponseEntity<Map<String, Object>> getUniversityCategoryByUniversityName(@RequestParam String universityName) {
+        List<University> universities = universityService.getUniversitiesByName(universityName);
+        University university = universities.stream().findFirst().orElseThrow(() -> new NotFoundEntityException("University not found with university name: " + universityName));
+        UniversityCategory category = universityService.getUniversityCategoryByUniversityName(universityName);
+        Map<String, Object> response = new HashMap<>();
+        response.put("University", university);
+        response.put("Category", category);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{universityId}/category")
@@ -55,10 +76,27 @@ public class UniversityController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/category")
-    @Operation(summary = "List all university categories", description = "모든 대학 카테고리 조회")
-    public ResponseEntity<List<UniversityCategory>> getAllUniversityCategories() {
-        return ResponseEntity.ok(universityService.getAllUniversityCategory());
+    @PostMapping(path = "/{universityId}/university", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new university", description = "새로운 대학 생성")
+    public ResponseEntity<University> createUniversity(@RequestParam String universityName, @RequestParam String universityDepartment) {
+        University university = universityService.createNewUniversity(universityName, universityDepartment);
+        return new ResponseEntity<>(university, HttpStatus.CREATED);
     }
+
+    @PutMapping(path = "/{universityId}/university", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Update an existing university by its university ID", description = "university id를 통한 대학 수정")
+    public ResponseEntity<University> updateUniversity(@PathVariable Long universityId, @RequestParam String universityName, @RequestParam String universityDepartment) {
+        University university = universityService.updateUniversityById(universityId, universityName, universityDepartment);
+        return ResponseEntity.ok(university);
+    }
+
+    @DeleteMapping(path = "/{universityId}/university")
+    @Operation(summary = "Delete a work by its university ID", description = "university id를 통한 대학 삭제")
+    public ResponseEntity<Void> deleteUniversity(@PathVariable Long universityId) {
+        universityService.deleteUniversity(universityId);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
--- glint/src/main/java/com/swyp/glint/keyword/application/UniversityService.java
+++ glint/src/main/java/com/swyp/glint/keyword/application/UniversityService.java
@@ -22,9 +22,17 @@ public class UniversityService {
                 .orElseThrow(() -> new NotFoundEntityException("University not found with id: " + universityId));
     }
 
-    public University findByName(String universityName) { // university name을 통한 University 엔티티 반환
-        return universityRepository.findByUniversityName(universityName)
+    public University findByName(String universityName, String universityDepartment) { // 대학명과 학과를 통한 university 반환
+        return universityRepository.findByUniversityNameAndUniversityDepartment(universityName, universityDepartment)
                 .orElseThrow(() -> new NotFoundEntityException("University not found with name: " + universityName));
+    }
+
+    public List<University> getUniversitiesByName(String universityName) { // 대학명을 통한 University 엔티티들 반환
+        List<University> universities = universityRepository.findByUniversityName(universityName);
+        if(universities.isEmpty()) {
+            throw new NotFoundEntityException("University not found with name: " + universityName);
+        }
+        return universities;
     }
 
     public List<University> getAllUniversity() { // 대학 전체 조회
@@ -36,13 +44,6 @@ public class UniversityService {
                 .map(University::getUniversityName)
                 .orElseThrow(() -> new NotFoundEntityException("University name not found with id: " + universityId));
     }
-
-    public Long getUniversityIdByName(String universityName) { // 대학명을 통한 university id 반환
-        return universityRepository.findByUniversityName(universityName)
-                .map(University::getId)
-                .orElseThrow(() -> new NotFoundEntityException("University id not found with name: " + universityName));
-    }
-
 
     public List<UniversityCategory> getAllUniversityCategory() { // 대학 카테고리 전체 조회
         return universityCategoryRepository.findAll();
@@ -56,9 +57,34 @@ public class UniversityService {
     }
 
     public UniversityCategory getUniversityCategoryByUniversityName(String universityName) { // 대학명을 통한 Category 엔티티 반환
-        University university = universityRepository.findByUniversityName(universityName)
-                .orElseThrow(() -> new NotFoundEntityException("University not found with name: " + universityName));
+        List<University> universities = universityRepository.findByUniversityName(universityName);
+        if (universities.isEmpty()) {
+            throw new NotFoundEntityException("universities not found with universityName: " + universityName);
+        }
+        University university = universities.stream().findFirst().orElseThrow(() -> new NotFoundEntityException("University not found with university name: " + universityName));
+        // 대학명을 통해 찾은 대학들의 리스트 중 하나의 University 엔티티만 추출하여 그 엔티티의 category id를 통해 Category 엔티티 반환
         return universityCategoryRepository.findById(university.getUniversityCategoryId())
                 .orElseThrow(() -> new NotFoundEntityException("University category not found for university: " + universityName));
+    }
+
+    public University createNewUniversity(String universityName, String universityDepartment) {
+        return universityRepository.findByUniversityNameAndUniversityDepartment(universityName, universityDepartment)
+                .orElseGet(() -> {
+                    University newUniversity = University.createNewUniversity(universityName, universityDepartment);
+                    return universityRepository.save(newUniversity);
+                });
+    }
+
+    public University updateUniversityById(Long universityId, String universityName, String universityDepartment) {
+        University university = universityRepository.findById(universityId)
+                .orElseThrow(() -> new NotFoundEntityException("University not found with university id: " + universityId));
+        university.updateUniversity(universityName, universityDepartment);
+        return  universityRepository.save(university);
+    }
+
+    public void deleteUniversity(Long universityId) {
+        University university = universityRepository.findById(universityId)
+                .orElseThrow(() -> new NotFoundEntityException("University not found with university id: " + universityId));
+        universityRepository.delete(university);
     }
 }
