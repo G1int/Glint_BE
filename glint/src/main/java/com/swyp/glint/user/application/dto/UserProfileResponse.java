c6abfb92ef3ab98da8439e1eddd54d73798bb7f9
yssy3135 <ys3135@gmail.com>
2024-07-21 15:33:03 +0900

    fix: #86: 유저 프로필 response 수정
    - 유저 프로필 response 수정
    - null 처리
    - 유저 정보 함께 반환하는 api 추가

--- glint/src/main/java/com/swyp/glint/user/application/dto/UserProfileResponse.java
+++ glint/src/main/java/com/swyp/glint/user/application/dto/UserProfileResponse.java
@@ -25,7 +25,7 @@ public class UserProfileResponse {
     private LocationResponse location;
     private ReligionResponse region;
     private SmokingResponse smoking;
-    private  DrinkingResponse drinking;
+    private DrinkingResponse drinking;
     @Schema(description = "자기소개", example = "안녕하세요 저는 서울 강북구에 사는 유재석이라고 합니다.")
     private  String selfIntroduction;
     @Schema(description = "나를 표현하는 키워드", example = "[적극적, ESTJ, 애교많음]")
