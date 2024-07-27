09cf9afff14e82f3f0234efcd728b0e9bea5d80c
yssy3135 <ys3135@gmail.com>
2024-07-28 01:08:21 +0900

    fix: 로그인시 회원가입, 기존회원 로그인 여부 추가
    - 로그인시 회원가입 인지, 기존회원인지 판별 불가
    - 회원가입, 기존회원 flag 추가

--- glint/src/main/java/com/swyp/glint/user/application/dto/UserLoginResponse.java
+++ glint/src/main/java/com/swyp/glint/user/application/dto/UserLoginResponse.java
@@ -2,20 +2,24 @@ package com.swyp.glint.user.application.dto;
 
 import com.swyp.glint.user.domain.User;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record UserLoginResponse(
-        @Parameter(description = "User ID", example = "1", required = true)
+        @Schema(description = "User ID", example = "1", required = true)
         Long id,
-        @Parameter(description = "User Email", example = "glint@gmail.com", required = true)
-        String email
+        @Schema(description = "User Email", example = "glint@gmail.com", required = true)
+        String email,
+        @Schema(description = "기존회원, 회원가입 여부", example = "true", required = true)
+        Boolean isSignUp
 ) {
 
-        public static UserLoginResponse from(User user) {
+        public static UserLoginResponse from(User user, Boolean isSignUp) {
             return UserLoginResponse.builder()
                     .id(user.getId())
                     .email(user.getEmail())
+                    .isSignUp(isSignUp)
                     .build();
         }
 }
